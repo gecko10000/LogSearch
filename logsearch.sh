e4aecf4feb8ae1b2df09538241b6412e93491ae7
gecko10000 <levtey@gmail.com>
2022-07-23 20:46:19 +0000

    Add script

---
+++ logsearch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+print_info() {
+  echo "Lines containing \"$1\" in $2:"
+}
+
+mapfile -t files < <(ls -- *.log.gz)
+for file in "${files[@]}"; do
+  print_info "$1" "$file"
+  echo "Lines containing \"$1\" in $file:"
+  gunzip -c -- "$file" | grep -ne "$1"
+done
+print_info "$1" "latest.log"
+grep -ne "$1" < latest.log
